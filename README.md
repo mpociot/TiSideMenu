d0aec77cd35b15049b8c2308f14eee41e6eeb658
Marcel Pociot <m.pociot@gmail.com>
2013-10-27 08:37:02 +0100

    Added documentation and proper example app.js

--- README.md
+++ README.md
@@ -11,11 +11,47 @@ Wrapper module for the great [RESideMenu](https://github.com/romaonthego/RESideM
 
 Usage
 ====================
-Please take a look at the example.js right now.
 
-Documentation will follow shortly.
+Use TiSideMenu as a replacement for your root window.
 
+	var contentView = Ti.UI.createWindow({
+		background: 'red'
+	});
+	
+	var menuView    = Ti.UI.createWindow({
+		background: 'transparent'
+	});
+	
+	var win = TiSideMenu.createSideMenu({
+		contentView: 		contentView,
+		menuView: 			menuView,
+		backgroundImage: 	'stars.png'
+	});
+	win.open();
 
+Configuration
+===
+	
+* Enable / Disable the pan gesture 
+	`win.setPanGestureEnabled( true / false );`
+	
+* Enable / Disable Parallax effect	
+	`win.setParallaxEnabled( true / false ); `
+	
+* Enable / Disable Background image scaling
+	`win.setScaleBackgroundImageView( true / false );`
+	
+* Enable / Disable Content view scaling
+	`win.setScaleContentView( true / false );`
+	
+* Set the content view scale value
+	`win.setContentViewScaleValue( 0.0 - 1.0 );`
+
+Manually showing / hiding the menu:
+
+`win.hideMenuViewController()`
+
+`win.presentMenuViewController()`
 
 
 ABOUT THE AUTHOR
--- example/app.js
+++ example/app.js
@@ -54,6 +54,96 @@ function createContentWindow()
 		win.presentMenuViewController();
 	});
 
+	// Module settings
+	scaleContentViewLabel = Ti.UI.createLabel({
+		text: 'Scale content view:',
+		top: 50,
+		left: 10
+	})
+	scaleContentViewBtn = Ti.UI.createSwitch({
+		value: true,
+		top: 50,
+		right: 10
+	});
+	contentWin.add( scaleContentViewLabel );
+	contentWin.add( scaleContentViewBtn );
+	scaleContentViewBtn.addEventListener('change',function(e)
+	{
+		win.setScaleContentView( scaleContentViewBtn.value );
+	});
+
+
+	scaleBackgroundImageViewLabel = Ti.UI.createLabel({
+		text: 'Scale background image:',
+		top: 100,
+		left: 10
+	})
+	scaleBackgroundImageViewBtn = Ti.UI.createSwitch({
+		value: true,
+		top: 100,
+		right: 10
+	});
+	contentWin.add( scaleBackgroundImageViewLabel );
+	contentWin.add( scaleBackgroundImageViewBtn );
+	scaleBackgroundImageViewBtn.addEventListener('change',function(e)
+	{
+		win.setScaleBackgroundImageView( scaleBackgroundImageViewBtn.value );
+	});
+
+	parallaxLabel = Ti.UI.createLabel({
+		text: 'Parallax enabled:',
+		top: 150,
+		left: 10
+	})
+	parallaxBtn = Ti.UI.createSwitch({
+		value: true,
+		top: 150,
+		right: 10
+	});
+	contentWin.add( parallaxLabel );
+	contentWin.add( parallaxBtn );
+	parallaxBtn.addEventListener('change',function(e)
+	{
+		win.setParallaxEnabled( parallaxBtn.value );
+	});
+
+
+	panLabel = Ti.UI.createLabel({
+		text: 'Pan gesture enabled:',
+		top: 200,
+		left: 10
+	});
+	panBtn = Ti.UI.createSwitch({
+		value: true,
+		top: 200,
+		right: 10
+	});
+	contentWin.add( panLabel );
+	contentWin.add( panBtn );
+	panBtn.addEventListener('change',function(e)
+	{
+		win.setPanGestureEnabled( panBtn.value );
+	});
+
+	scaleLabel = Ti.UI.createLabel({
+		text: 'Content View scale:',
+		top: 250,
+		left: 10
+	})
+	var scaleSlider = Titanium.UI.createSlider({
+    	top: 290,
+    	min: 0,
+    	max: 100,
+    	width: '100%',
+    	value: 50
+    });
+	contentWin.add( scaleLabel );
+	contentWin.add( scaleSlider );
+	scaleSlider.addEventListener('change', function(e) {
+	    win.setContentViewScaleValue( e.value / 100 );
+	});
+
+
 	var navController = Ti.UI.iOS.createNavigationWindow({
 		window : contentWin
 	});
